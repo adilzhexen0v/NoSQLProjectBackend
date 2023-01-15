124210439a928ff7d53aca61c45749a77c428d2a
adilzhexenov <nescrypnoadil@gmail.com>
2023-01-15 23:17:51 +0600

    change userModel

--- models/User.js
+++ models/User.js
@@ -19,6 +19,8 @@ const UserSchema = new mongoose.Schema({
           required: true,
      },
      dateOfBirth: String,
+     phoneNumber: String,
+     gender: String,
      avatarUrl: String,
 },
 {
